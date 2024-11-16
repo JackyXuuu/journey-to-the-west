70253fb9966527d0357fa39834ffe64b02ca8d63
plexity <35226662+anthonyjuan-wang@users.noreply.github.com>
2024-11-16 22:54:55 +0800

    cooldown timer

--- gui/hud/hud.gd
+++ gui/hud/hud.gd
@@ -7,7 +7,13 @@ extends Control
 		"stats": preload("res://resources/ally_mob_1.tres")
 	}, 
 }
+
+@export var AllyMob1: Button
+@export var AllyMob2: Button
+
+
 var essence: int = 0
+var cooldowns: Dictionary = {}
 
 signal spawn_mob(mob_scene: PackedScene)
 
@@ -20,18 +26,74 @@ func _on_pressed(mob_key: String) -> void:
 		var mob_info = mob_data[mob_key]
 		var mob_scene = mob_info["scene"]
 		var mob_stats = mob_info["stats"]
+		if not is_cooldown_over(mob_key):
+			print("Unit is on cooldown")
+			return
+		
 		if essence >= mob_stats.summon_cost:
-			spawn_mob.emit(mob_scene)
+			start_cooldown(mob_key, mob_stats.cooldown)
 			essence -= mob_stats.summon_cost
 			update_essence_display()
-	
+			spawn_mob.emit(mob_scene)
 	else:
 		print("Invalid mob key")
+		
 func update_essence_display():
-	print("updating")
 	$MarginContainer/HBoxContainer/EssenceLabel.text = "x " + str(essence)  # Display essence on a label
 
 func _on_essence_timer_timeout() -> void:
-	print("hello123")
 	essence += Global.essence_per_second
 	update_essence_display()
+	
+func start_cooldown(mob_key: String, cooldown_time: float) -> void:
+	var button
+	match mob_key:
+		"AllyMob1":
+			button = AllyMob1
+		"AllyMob2":
+			button = AllyMob2
+	button.disabled = true
+	cooldowns[button] = cooldown_time
+
+	if button.has_node("CooldownLabel"):
+		var label = button.get_node("CooldownLabel")
+		label.visible = true
+		label.text = str(ceil(cooldown_time))
+	var cooldown_timer = Timer.new()
+	cooldown_timer.one_shot = false
+	cooldown_timer.wait_time = 0.1
+	cooldown_timer.connect("timeout", Callable(self, "_update_cooldown").bind(button, cooldown_timer))
+	add_child(cooldown_timer)
+	cooldown_timer.start()
+		# disable button
+
+func is_cooldown_over(mob_key: String) -> bool:
+	if mob_key in cooldowns:
+		return Time.get_ticks_msec() >= cooldowns[mob_key]
+	return true
+
+func _on_cooldown_complete(button: Button, mob_key: String) -> void:
+	button.disabled = false
+	button.text = ""
+	if mob_key in cooldowns:
+		cooldowns.erase(mob_key)
+
+func _update_cooldown(button: Button, timer: Timer):
+	if not button in cooldowns:
+		return
+
+	cooldowns[button] -= 0.1
+
+	# Update cooldown label
+	if button.has_node("CooldownLabel"):
+		var label = button.get_node("CooldownLabel")
+		label.text = str(ceil(cooldowns[button]))
+
+	# End cooldown
+	if cooldowns[button] <= 0:
+		cooldowns.erase(button)
+		button.disabled = false
+		if button.has_node("CooldownLabel"):
+			button.get_node("CooldownLabel").visible = false
+		timer.stop()
+		timer.queue_free()
